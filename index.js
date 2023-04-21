4ba44bf37f197e78ac5f4bf5a996fb4b500d3e49
NIPUN <nipunchanakara@gmail.com>
2023-04-21 21:07:26 +0530

    callback-function

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+//asychronous
+
 console.log("Before");
-setTimeout(() => console.log("reading use from a database"), 2000);
+getUser(1, (user) => {
+  console.log("User", user);
+  //get the repositories
+  getRepositories(user.gitHubUsername, (repos) => {
+    console.log("repos", repos);
+    //Call basck hell
+  });
+});
+//console.log(user);
 console.log("After");
+
+//call back
+//promises
+//Async/await
+
+// //sychronous
+
+// console.log("Before");
+// const user = getUser(1);
+// const repos = getRepositories(user.gitHubUsername);
+// console.log("After");
+
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log("reading use from a database");
+    callback({ id: id, gitHubUsername: "nipun" });
+  }, 2000);
+}
+
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    console.log("calling API...");
+    callback(["repo1", "repo2", "repo3"]);
+  });
+}
