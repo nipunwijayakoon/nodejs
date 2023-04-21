73a52fa5710329075eefa31ccb71e6740d5d9867
NIPUN <nipunchanakara@gmail.com>
2023-04-22 03:14:08 +0530

    await-imlemented

--- promise-api.js
+++ promise-api.js
@@ -1,16 +1,54 @@
-const p1 = new Promise((resolve) => {
-  setTimeout(() => {
-    console.log("Async operation 1...");
-    resolve(1);
-  }, 2000);
-});
+console.log("Before");
+// getUser(1, getRepositories);
+// //console.log(user);
+console.log("After");
 
-const p2 = new Promise((resolve) => {
-  setTimeout(() => {
-    console.log("Async operation 2...");
-    resolve(2);
-  }, 2000);
-});
+// getUser(1)
+//   .then((user) => getRepositories(user.gitHubUsername))
+//   .then((repos) => getCommits(repos[0]))
+//   .then((commits) => console.log("commits", commits))
+//   .catch((err) => console.log("Error"));
 
-Promise.all([p1, p2]).then((result) => console.log(result));
-// all wenuwata race demmoth mulin fullfil wena result eka gnn puluwan
+//Async and awit aprroach
+async function displayCommits() {
+  try {
+    const user = await getUser(1);
+    const repos = await getRepositories(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  } catch (err) {
+    console.log("Error", err.message);
+  }
+}
+
+displayCommits();
+
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    //kick off some async work
+    setTimeout(() => {
+      console.log("reading use from a database");
+      resolve({ id: id, gitHubUsername: "nipun" });
+    }, 2000);
+  });
+}
+
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    //kick off some async work
+    setTimeout(() => {
+      console.log("calling API...");
+      resolve(["repo1", "repo2", "repo3"]);
+    }, 2000);
+  });
+}
+
+function getCommits(repo) {
+  return new Promise((resolve, reject) => {
+    //kick off some async work
+    setTimeout(() => {
+      console.log("calling  GithubAPI...");
+      resolve(["commit"]);
+    }, 2000);
+  });
+}
