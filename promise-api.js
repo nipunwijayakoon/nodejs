6bc073aaf8fb924b021122127304306c099ee920
NIPUN <nipunchanakara@gmail.com>
2023-04-22 02:42:04 +0530

    parallel-promise-completed

--- promise-api.js
+++ promise-api.js
@@ -1,6 +1,16 @@
-//promises create already resolve
-const p = Promise.resolve({ id: 1 });
-p.then((result) => console.log(result));
-//promises create already rejected
-const q = Promise.reject(new Error("reason for rejection..."));
-q.catch((error) => console.log(error));
+const p1 = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log("Async operation 1...");
+    resolve(1);
+  }, 2000);
+});
+
+const p2 = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log("Async operation 2...");
+    resolve(2);
+  }, 2000);
+});
+
+Promise.all([p1, p2]).then((result) => console.log(result));
+// all wenuwata race demmoth mulin fullfil wena result eka gnn puluwan
